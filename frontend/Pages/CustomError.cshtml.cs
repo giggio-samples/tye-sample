cc19a5a8931678c1005bc74bcaf88dd477a3dcd4
Giovanni Bassi <giggio@giggio.net>
2020-10-29 23:18:29 -0300

    Add logging with Seq

---
+++ frontend/Pages/CustomError.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace frontend.Pages
+{
+    public class CustomErrorModel : PageModel
+    {
+        private readonly ILogger<CustomErrorModel> _logger;
+
+        public CustomErrorModel(ILogger<CustomErrorModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnGet()
+        {
+            _logger.LogError("A custom error, logged.");
+            throw new ApplicationException("A custom error");
+        }
+    }
+}
